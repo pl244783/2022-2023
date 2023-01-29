8df56706ea3a93a2917af08c92ff207396f399e9
pl244783 <pl244783@ahschool.com>
2023-01-29 11:25:13 -0500

    poggers

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ def forward():
     speedR = int(request.args.get('speedR', default = 64))
     timeMS = int(request.args.get('timeMS', default = 1000))
     #robot.motorForward(speedL, speedR, timeMS)
-    print("forward")
     return "<p>forward</p>"
 
 
@@ -28,7 +27,6 @@ def backward():
     speedR = int(request.args.get('speedR', default = 66))
     timeMS = int(request.args.get('timeMS', default = 1000))
     #robot.motorBackward(speedL, speedR, timeMS)
-    print("backward")
     return "<p>backward</p>"
 
 @app.route("/left", methods = ['GET'])
@@ -36,7 +34,6 @@ def left():
     speedL = int(request.args.get('speedL', default = 50))
     speedR = int(request.args.get('speedR', default = 60))
     timeMS = int(request.args.get('timeMS', default = 850))
-    print("left")
     #robot.motorLeft(speedL, speedR, timeMS)
     return "<p>left</p>"
 
@@ -46,7 +43,6 @@ def right():
     speedR = int(request.args.get('speedR', default = 60))
     timeMS = int(request.args.get('timeMS', default = 850))
     #robot.motorRight(speedL, speedR, timeMS)
-    print("right")
     return "<p>right</p>"
 
 if __name__=='__main__':
