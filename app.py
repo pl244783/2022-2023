31103a06e2d6c092ed269b4803887df16b77fed4
pl244783 <pl244783@ahschool.com>
2023-01-28 19:31:15 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, render_template
+#from robotLibrary import Robot
+
+app = Flask(__name__)
+#robot = Robot()
+
+#run flask run --host=0.0.0.0
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+ 
+@app.route("/forward", methods = ['GET'])
+def forward():
+    speedL = int(request.args.get('speedL', default = 50))
+    speedR = int(request.args.get('speedR', default = 64))
+    timeMS = int(request.args.get('timeMS', default = 1000))
+    #robot.motorForward(speedL, speedR, timeMS)
+    return "<p>forward</p>"
+
+
+@app.route("/backward", methods = ['GET'])
+def backward():
+    speedL = int(request.args.get('speedL', default = 50))
+    speedR = int(request.args.get('speedR', default = 66))
+    timeMS = int(request.args.get('timeMS', default = 1000))
+    #robot.motorBackward(speedL, speedR, timeMS)
+    return "<p>backward</p>"
+
+@app.route("/left", methods = ['GET'])
+def left():
+    speedL = int(request.args.get('speedL', default = 50))
+    speedR = int(request.args.get('speedR', default = 60))
+    timeMS = int(request.args.get('timeMS', default = 850))
+    #robot.motorLeft(speedL, speedR, timeMS)
+    return "<p>left</p>"
+
+@app.route("/right", methods = ['GET'])
+def right():
+    speedL = int(request.args.get('speedL', default = 50))
+    speedR = int(request.args.get('speedR', default = 60))
+    timeMS = int(request.args.get('timeMS', default = 850))
+    #robot.motorRight(speedL, speedR, timeMS)
+    return "<p>right</p>"
+
+if __name__=='__main__':
+    app.run(debug = True)
