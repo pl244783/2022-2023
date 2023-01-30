54b56d2623a66b3f63db6bd261f45e53f85110dc
pl244783 <pl244783@ahschool.com>
2023-01-29 21:17:08 -0500

    ur mom

--- app.py
+++ app.py
@@ -57,7 +57,6 @@ def stream():
             while True:
                 yield f.read()
                 sleep(1)
-
     return app.response_class(generate(), mimetype="text/plain")
 
 if __name__=='__main__':
