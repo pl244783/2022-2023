d8f2939e49fe873018e2723cab8cd588e98a3ba2
Michael Wang <113716608+pl244783@users.noreply.github.com>
2023-03-16 08:45:27 -0400

    everything sucks :(

--- parallelLineDetector.py
+++ parallelLineDetector.py
@@ -1,8 +1,10 @@
 import cv2
 import numpy as np
+import math
 
 # Load the photo
-img = cv2.imread('working1.jpg')
+#use 1, 2, 6
+img = cv2.imread('working6.jpg')
 
 # Convert the photo to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -11,7 +13,7 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray, 50, 150, apertureSize=3)
 
 # Apply Hough Transform to detect lines
-lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi/180, threshold=50, minLineLength=3000, maxLineGap=1000)
+lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi/180, threshold=50, minLineLength=2000, maxLineGap=1000)
 
 # Find pairs of parallel lines
 parallel_lines = []
@@ -29,6 +31,12 @@ for i in range(len(lines)):
 for line in parallel_lines:
     cv2.line(img, (line[0][0], line[0][1]), (line[0][2], line[0][3]), (0, 255, 0), 2)
     cv2.line(img, (line[1][0], line[1][1]), (line[1][2], line[1][3]), (0, 255, 0), 2)
+
+print((int(line[1][3])-int(line[0][3]))**2)
+print(math.sqrt(int(line[1][3])-int(line[0][3]))**2)
+
+cv2.line(img, (math.sqrt((int(line[1][0])-int(line[0][0]))**2), math.sqrt((int(line[1][1])-int(line[0][1]))**2)),
+         (math.sqrt((int(line[1][2])-int(line[0][2]))**2), math.sqrt((int(line[1][3])-int(line[0][3])**2))), (0, 0, 255), 2)
 
 #comment this out when on school computer
 img = cv2.resize(img, dsize=(500,500))
