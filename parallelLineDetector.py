a72c03e627673004789f051e26727939d129a224
Michael Wang <113716608+pl244783@users.noreply.github.com>
2023-03-16 09:04:26 -0400

    possible stinker fix

--- parallelLineDetector.py
+++ parallelLineDetector.py
@@ -33,7 +33,7 @@ for line in parallel_lines:
     cv2.line(img, (line[1][0], line[1][1]), (line[1][2], line[1][3]), (0, 255, 0), 2)
 
 print((int(line[1][3])-int(line[0][3]))**2)
-print(math.sqrt(int(line[1][3])-int(line[0][3]))**2)
+print(math.sqrt((int(line[1][3])-int(line[0][3]))**2))
 
 cv2.line(img, (math.sqrt((int(line[1][0])-int(line[0][0]))**2), math.sqrt((int(line[1][1])-int(line[0][1]))**2)),
          (math.sqrt((int(line[1][2])-int(line[0][2]))**2), math.sqrt((int(line[1][3])-int(line[0][3])**2))), (0, 0, 255), 2)
