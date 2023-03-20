18ac02e9947d6c5442e34543dfb2783b6f81dd60
pl244783 <pl244783@ahschool.com>
2023-03-19 23:27:19 -0400

    i have no clue why test.py works, but it does so idk :/

---
+++ test.py
@@ -0,0 +1,61 @@
+import cv2
+import numpy as np
+
+# Load the photo
+#use 1, 2, 6
+img = cv2.imread('test.jpg')
+
+# Convert the photo to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Apply Canny edge detection
+edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+
+# Apply Hough Transform to detect lines
+lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi/180, threshold=100, minLineLength=200, maxLineGap=1000)
+
+# Find pairs of parallel lines
+parallel_lines = []
+for i in range(len(lines)):
+    for j in range(i+1, len(lines)):
+        line1 = lines[i][0]
+        line2 = lines[j][0]
+        angle1 = np.arctan2(line1[1]-line1[3], line1[0]-line1[2]) * 180 / np.pi
+        angle2 = np.arctan2(line2[1]-line2[3], line2[0]-line2[2]) * 180 / np.pi
+        if np.abs(angle1 - angle2) < 5:
+            parallel_lines.append((line1, line2))
+            print(line1, line2)
+
+def draw_line(img, x1, y1, x2, y2, color, thickness):
+    # Check if there is a line within +/- 1000 pixels in x and y directions
+    x1_nearby, y1_nearby, x2_nearby, y2_nearby = False, False, False, False
+    for other_line in parallel_lines:
+        if 100 < abs(other_line[0][0] - x1) < 1000 and 100 < abs(other_line[0][1] - y1) < 1000 and 100 < abs(other_line[0][2] - x2) < 1000 and 100 < abs(other_line[0][3] - y2) < 1000:
+            x1_nearby, y1_nearby, x2_nearby, y2_nearby = True, True, True, True
+            break
+    
+    # If there is a nearby line, change the color to red
+    if x1_nearby and y1_nearby and x2_nearby and y2_nearby:
+        color = (0, 255, 0)
+    
+    cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+
+# Draw the detected lines on the photo
+for line in parallel_lines:
+    cv2.line(img, (line[0][0], line[0][1]), (line[0][2], line[0][3]), (0, 255, 0), 2)
+    cv2.line(img, (line[1][0], line[1][1]), (line[1][2], line[1][3]), (0, 255, 0), 2)
+    draw_line(img, int((line[0][0]+line[1][0])/2), int((line[0][1]+line[1][1])/2), int((line[0][2]+line[1][2])/2), int((line[0][3]+line[1][3])/2), (0, 0, 255), 2)
+#cv2.line(img, (int((line[0][0]+line[1][0])/2), int((line[0][1]+line[1][1])/2)), (int((line[0][2]+line[1][2])/2), int((line[0][3]+line[1][3])/2)), (0, 255, 255), 2)
+
+# cv2.line(img, (line1[0], line1[1]), (line1[2], line1[3]), (0, 0, 255), 5)
+# cv2.line(img, (line2[0], line2[1]), (line2[2], line2[3]), (0, 0, 255), 5)
+
+#print((int((line[0][0]+line[1][0])/2), int((line[0][1]+line[1][1])/2)), line1, line2)
+
+#comment this out when on school computer
+img = cv2.resize(img, dsize=(900,900))
+
+# Show the result
+cv2.imshow('Result', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
