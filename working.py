a4feb28e6e0ee6eb4bca226ef8c536fe13f72b3a
pl244783 <pl244783@ahschool.com>
2023-04-01 16:24:55 -0400

    pogcrazy pochy acceptance

--- working.py
+++ working.py
@@ -16,7 +16,7 @@ edges = cv2.Canny(gray, 50, 150, apertureSize=3)
 lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi/180, threshold=60, minLineLength=0, maxLineGap=400)
 
 # Find pairs of parallel lines
-parallel_lines = []
+parallelLines = []
 for i in range(len(lines)):
     for j in range(i+1, len(lines)):
         line1 = lines[i][0]
@@ -24,14 +24,14 @@ for i in range(len(lines)):
         angle1 = np.arctan2(line1[1]-line1[3], line1[0]-line1[2]) * 180 / np.pi
         angle2 = np.arctan2(line2[1]-line2[3], line2[0]-line2[2]) * 180 / np.pi
         if np.abs(angle1 - angle2) < 5:
-            parallel_lines.append((line1, line2))
+            parallelLines.append((line1, line2))
             print(line1, line2)
 
 def draw_line(img, x1, y1, x2, y2, colour, thickness):
     #var def uwu
     nearBy = False
     #checks for idk tbh close lines maybe
-    for other_line in parallel_lines:
+    for other_line in parallelLines:
         if (abs(other_line[0][0] - x1) < 100 and abs(other_line[0][1] - y1) < 100) or (abs(other_line[0][2] - x2) < 100 and abs(other_line[0][3] - y2) < 100):
             nearBy = True
             break
@@ -40,7 +40,7 @@ def draw_line(img, x1, y1, x2, y2, colour, thickness):
     cv2.line(img, (x1, y1), (x2, y2), colour, thickness)
 
 # Draw the detected lines on the photo
-for line in parallel_lines:
+for line in parallelLines:
     cv2.line(img, (line[0][0], line[0][1]), (line[0][2], line[0][3]), (0, 255, 0), 2)
     cv2.line(img, (line[1][0], line[1][1]), (line[1][2], line[1][3]), (0, 255, 0), 2)
     draw_line(img, int((line[0][0]+line[1][0])/2), int((line[0][1]+line[1][1])/2), int((line[0][2]+line[1][2])/2), int((line[0][3]+line[1][3])/2), (0, 0, 255), 2)
