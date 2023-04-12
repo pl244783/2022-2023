0fa78f130f46391a0fe8b7d65b544f32c174044c
pl244783 <pl244783@ahschool.com>
2023-04-11 21:02:02 -0400

    I do not understand, nor will I ever understand, but hopefully it'll work

--- lineDetecter.py
+++ lineDetecter.py
@@ -5,13 +5,13 @@ img = cv2.imread('stockphoto.jpg')
 img = cv2.GaussianBlur(img, (7, 7), 0)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray, 120, 200, apertureSize=3)
-# lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi/180, threshold=300, minLineLength=0, maxLineGap=10)
+lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi/180, threshold=30, minLineLength=50, maxLineGap=10)
 
-# for line in lines:
-#     x1, y1, x2, y2 = line[0]
-#     cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+for line in lines:
+    x1, y1, x2, y2 = line[0]
+    cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
-img = cv2.resize(edges, dsize=(500,500))
+img = cv2.resize(img, dsize=(500,500))
 cv2.imshow('Result', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
