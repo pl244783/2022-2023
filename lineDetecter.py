9c06c5b3788b5d0c867f809328f030ebf175ed46
pl244783 <pl244783@ahschool.com>
2023-04-11 20:57:43 -0400

    I'm in hell, but I'm clawing my way out

---
+++ lineDetecter.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('stockphoto.jpg')
+img = cv2.GaussianBlur(img, (7, 7), 0)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray, 120, 200, apertureSize=3)
+# lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi/180, threshold=300, minLineLength=0, maxLineGap=10)
+
+# for line in lines:
+#     x1, y1, x2, y2 = line[0]
+#     cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+img = cv2.resize(edges, dsize=(500,500))
+cv2.imshow('Result', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
