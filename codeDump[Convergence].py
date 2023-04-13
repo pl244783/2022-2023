7721c70371a9482edfbb2f79d4d7f31c73311c52
Michael Wang <pl244783@ahschool.com>
2023-04-13 08:26:20 -0400

    Convergence 2

--- codeDump[Convergence].py
+++ codeDump[Convergence].py
@@ -13,11 +13,20 @@ refPointOne = [int(img.shape[1]/2), int(img.shape[0]/2), int(img.shape[1]/4), in
 refPointTwo = [int(img.shape[1]/2), int(img.shape[0]/2), int(img.shape[1]/4)*3, int(img.shape[0])]
 
 def nearBy(x1, y1, x2, y2):
-    if abs(refPointOne - x1)
+    #left
+    if (abs(refPointOne[2] - x1) < 200 and abs(refPointOne[3] - y1) < 200) or (abs(refPointOne[2] - x2) < 200 and abs(refPointOne[3] - y2) < 200):
+        cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 1) 
+        return 0
+    
+    #right
+    if (abs(refPointTwo[2] - x1) < 200 and abs(refPointTwo[3] - y1) < 200) or (abs(refPointTwo[2] - x2) < 200 and abs(refPointTwo[3] - y2) < 200):
+        cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 1) 
+        return 0
 
 for line in lines:
     x1, y1, x2, y2 = line[0]
-    cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 1)
+    #cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 1)
+    nearBy(x1, y1, x2, y2)
     print(x1, x2, y1, y2)
 
 cv2.line(img, (midPointCoord[0], midPointCoord[1]), (midPointCoord[2], midPointCoord[3]), (255, 0, 0), 1)
