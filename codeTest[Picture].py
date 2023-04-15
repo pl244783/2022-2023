03366793c091e060d688301217f9a5bfcad9becf
pl244783 <pl244783@ahschool.com>
2023-04-15 13:57:05 -0400

    GitHubPhoto Success

--- codeTest[Picture].py
+++ codeTest[Picture].py
@@ -1,25 +1,26 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('roadPictures/homePhoto1.jpg')
+#all Github imgs work
+img = cv2.imread('roadPictures/gitHubPhoto3.jpg')
 img = cv2.GaussianBlur(img, (5, 5), 0)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray, 120, 200, apertureSize=3, L2gradient = True)
-lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi/180, threshold=50, minLineLength=7, maxLineGap=100)
+edges = cv2.Canny(gray, 120, 200, apertureSize=3)
+lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi/180, threshold=50, minLineLength=0, maxLineGap=50)
 
 #theoretical perfect
-midPointCoord = [int(img.shape[1]/2), int(img.shape[0]/2), int(img.shape[1]/2), int(img.shape[0])]
-refPointOne = [int(img.shape[1]/2)-10, int(img.shape[0]/2), int(img.shape[1]/4), int(img.shape[0])]
-refPointTwo = [int(img.shape[1]/2)+10, int(img.shape[0]/2), int(img.shape[1]/4)*3, int(img.shape[0])]
+midPointCoord = [int(img.shape[1]/2), int(img.shape[0]/2)+int(img.shape[0]/8), int(img.shape[1]/2), int(img.shape[0])]
+refPointOne = [int(img.shape[1]/2)-int(img.shape[1]/25), int(img.shape[0]/2)+int(img.shape[0]/8), int(img.shape[1]/4), int(img.shape[0])]
+refPointTwo = [int(img.shape[1]/2)+int(img.shape[1]/25), int(img.shape[0]/2)+int(img.shape[0]/8), int(img.shape[1]/4)*3, int(img.shape[0])]
 
 def nearBy(x1, y1, x2, y2):
     #left
-    if (abs(refPointOne[2] - x1) < 200 and abs(refPointOne[3] - y1) < 200) or (abs(refPointOne[2] - x2) < 200 and abs(refPointOne[3] - y2) < 200):
+    if (abs(refPointOne[2] - x1) < 150 and abs(refPointOne[3] - y1) < 150) or (abs(refPointOne[2] - x2) < 150 and abs(refPointOne[3] - y2) < 150):
         #cv2.line(img, (x1, y1), (x2, y2), (0, 255, 255), 1) 
         nearTrueMid(x1, y1, x2, y2)
     
     #right
-    if (abs(refPointTwo[2] - x1) < 200 and abs(refPointTwo[3] - y1) < 200) or (abs(refPointTwo[2] - x2) < 200 and abs(refPointTwo[3] - y2) < 200):
+    if (abs(refPointTwo[2] - x1) < 150 and abs(refPointTwo[3] - y1) < 150) or (abs(refPointTwo[2] - x2) < 150 and abs(refPointTwo[3] - y2) < 150):
         #cv2.line(img, (x1, y1), (x2, y2), (0, 255, 255), 1) 
         nearTrueMid(x1, y1, x2, y2)
     
@@ -33,11 +34,11 @@ for line in lines:
     print(x1, x2, y1, y2)
 
 #reference area
-# cv2.line(img, (midPointCoord[0], midPointCoord[1]), (midPointCoord[2], midPointCoord[3]), (255, 0, 0), 1)
-# cv2.line(img, (refPointOne[0], refPointOne[1]), (refPointOne[2], refPointOne[3]), (0, 255, 0), 1)
-# cv2.line(img, (refPointTwo[0], refPointTwo[1]), (refPointTwo[2], refPointTwo[3]), (0, 255, 0), 1)
+cv2.line(img, (midPointCoord[0], midPointCoord[1]), (midPointCoord[2], midPointCoord[3]), (255, 0, 0), 1)
+cv2.line(img, (refPointOne[0], refPointOne[1]), (refPointOne[2], refPointOne[3]), (0, 255, 0), 1)
+cv2.line(img, (refPointTwo[0], refPointTwo[1]), (refPointTwo[2], refPointTwo[3]), (0, 255, 0), 1)
 
-img = cv2.resize(edges, dsize=(500,500))
+#img = cv2.resize(edges, dsize=(500,500))
 cv2.imshow('Result', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- codeTest[Video].py
+++ codeTest[Video].py
@@ -4,18 +4,20 @@ import numpy as np
 def nearBy(x1, y1, x2, y2):
     #left
     if (abs(refPointOne[2] - x1) < 150 and abs(refPointOne[3] - y1) < 150) or (abs(refPointOne[2] - x2) < 150 and abs(refPointOne[3] - y2) < 150):
-        #cv2.line(img, (x1, y1), (x2, y2), (0, 255, 255), 1) 
         nearTrueMid(x1, y1, x2, y2)
     
     #right
     elif (abs(refPointTwo[2] - x1) < 150 and abs(refPointTwo[3] - y1) < 150) or (abs(refPointTwo[2] - x2) < 150 and abs(refPointTwo[3] - y2) < 150):
-        #cv2.line(img, (x1, y1), (x2, y2), (0, 255, 255), 1) 
         nearTrueMid(x1, y1, x2, y2)
+        
     
 def nearTrueMid(x1, y1, x2, y2):
+    #cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 255), 3)
     if (abs(midPointCoord[0] - x1) < 50 and abs(midPointCoord[1] - y1) < 50) or (abs(midPointCoord[0] - x2) < 50 and abs(midPointCoord[1] - y2) < 50):
         cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 3) 
+ 
 
+#1 sucks, 2 is tentative, 3 is trash
 cap = cv2.VideoCapture('roadVideos/gitHubVideo1.mp4')
 
 while cap.isOpened():
@@ -27,18 +29,25 @@ while cap.isOpened():
     refPointTwo = [int(frame.shape[1]/2)+10, int(frame.shape[0]/2), int(frame.shape[1]/4)*3, int(frame.shape[0])]
     
     if ret:
+        frame = cv2.GaussianBlur(frame, (5, 5), 0)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        edges = cv2.Canny(gray, 120, 200, apertureSize=3, L2gradient = True)
-        lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi/180, threshold=50, minLineLength=7, maxLineGap=100)
+        edges = cv2.Canny(gray, 120, 200, apertureSize=3)
+        lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi/180, threshold=50, minLineLength=0, maxLineGap=100)
 
         for line in lines:
             x1, y1, x2, y2 = line[0]
             nearBy(x1, y1, x2, y2)
             print(x1, x2, y1, y2)
+        
+        #reference area
+        cv2.line(frame, (midPointCoord[0], midPointCoord[1]), (midPointCoord[2], midPointCoord[3]), (255, 0, 0), 1)
+        cv2.line(frame, (refPointOne[0], refPointOne[1]), (refPointOne[2], refPointOne[3]), (0, 255, 0), 1)
+        cv2.line(frame, (refPointTwo[0], refPointTwo[1]), (refPointTwo[2], refPointTwo[3]), (0, 255, 0), 1)
 
-        cv2.imshow("Circle Detection", frame)
+        frame = cv2.resize(frame, dsize = (500, 500))
+        cv2.imshow("Road Detection", frame)
 
-        if cv2.waitKey(100) & 0xFF == ord('q'):
+        if cv2.waitKey(10) & 0xFF == ord('q'):
             break
     else:
         break
