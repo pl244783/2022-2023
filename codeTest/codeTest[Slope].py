65a871ac67cbdddd244619a104d1d87c14801bea
pl244783 <pl244783@ahschool.com>
2023-04-15 22:15:30 -0400

    Proper

--- codeTest/codeTest[Slope].py
+++ codeTest/codeTest[Slope].py
@@ -5,10 +5,12 @@ def nearBy(x1, y1, x2, y2):
     #left
     if (abs(refPointOne[2] - x1) < 100 and abs(refPointOne[3] - y1) < 150) or (abs(refPointOne[2] - x2) < 100 and abs(refPointOne[3] - y2) < 150):
         nearTrueMid(x1, y1, x2, y2, refPointOne[0])
+        return 'left'
     
     #right
     elif (abs(refPointTwo[2] - x1) < 100 and abs(refPointTwo[3] - y1) < 150) or (abs(refPointTwo[2] - x2) < 100 and abs(refPointTwo[3] - y2) < 150):
         nearTrueMid(x1, y1, x2, y2, refPointTwo[0])
+        return 'right'
         
 def nearTrueMid(x1, y1, x2, y2, direction):
     #cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 255), 3)
@@ -21,6 +23,8 @@ def nearTrueMid(x1, y1, x2, y2, direction):
             else: 
                 cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 2) 
  
+def slopeCheck(x1, y1, x2, y2):
+    return round(abs(y1-y2)/abs(x1-x2), 2)
 
 #1 sucks, 2 is tentative, 3 is trash
 cap = cv2.VideoCapture('roadVideos/gitHubVideo1.mp4')
@@ -34,15 +38,16 @@ while cap.isOpened():
         refPointOne = [int(frame.shape[1]/2)-int(frame.shape[1]/25), int(frame.shape[0]/2)+int(frame.shape[0]/8), int(frame.shape[1]/4), int(frame.shape[0])]
         refPointTwo = [int(frame.shape[1]/2)+int(frame.shape[1]/25), int(frame.shape[0]/2)+int(frame.shape[0]/8), int(frame.shape[1]/4)*3, int(frame.shape[0])]
 
-        frame = cv2.GaussianBlur(frame, (9, 9), 0)
+        frame = cv2.GaussianBlur(frame, (7, 7), 0)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         edges = cv2.Canny(gray, 120, 200, apertureSize=3)
         lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi/180, threshold=50, minLineLength=0, maxLineGap=1500)
 
         for line in lines:
             x1, y1, x2, y2 = line[0]
-            nearBy(x1, y1, x2, y2)
-            print(x1, x2, y1, y2)
+            placeHolder = nearBy(x1, y1, x2, y2)
+            if placeHolder is not None:
+                print(x1, y1, x2, y2, placeHolder, slopeCheck(x1, y1, x2, y2))
         
         #reference area
         # cv2.line(frame, (midPointCoord[0], midPointCoord[1]), (midPointCoord[2], midPointCoord[3]), (255, 0, 0), 1)
@@ -52,7 +57,7 @@ while cap.isOpened():
         frame = cv2.resize(frame, dsize = (500, 500))
         cv2.imshow("Road Detection", frame)
 
-        if cv2.waitKey(10) & 0xFF == ord('q'):
+        if cv2.waitKey(100) & 0xFF == ord('q'):
             break
     else:
         break
