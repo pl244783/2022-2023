92624fd0d7a40b9c9bbf3a1914a73925d64028b7
pl244783 <pl244783@ahschool.com>
2023-04-20 18:11:51 -0400

    HTML Confirmation

--- HTMLFiles/newapp.py
+++ HTMLFiles/newapp.py
@@ -11,7 +11,8 @@ app.secret_key = 'your-secret-key-here'
 
 class ExcludeFilter(logging.Filter):
     def filter(self, record):
-        return "GET /static/logFile.html" not in record.getMessage()
+        message = record.getMessage()
+        return "GET /static/logFile.html" not in message and "GET /video_feed?rand=" not in message
 
 logger = logging.getLogger()
 handler = logging.FileHandler('HTMLFiles/static/logFile.html', 'w')
@@ -96,14 +97,83 @@ def register():
         return render_template('register.html')
     
 def gen_frames():
-        cap = cv2.VideoCapture('codeFiles/roadVideos/gitHubVideo1.mp4')
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if ret:
-                _, buffer = cv2.imencode('.jpg', frame)
-                frame = buffer.tobytes()
-                yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    #I don't know why my program needs this function, but when I try to delete it, it doesn't work anymore
+    def nearBy(x1, y1, x2, y2):
+        #left
+        if (abs(refPointOne[2] - x1) < frame.shape[0]/5 and abs(refPointOne[3] - y1) < frame.shape[0]/5) or (abs(refPointOne[2] - x2) < frame.shape[0]/5 and abs(refPointOne[3] - y2) < frame.shape[0]/5):
+            return nearTrueMid(x1, y1, x2, y2, (0, 0, 255))
+        
+        #right
+        elif (abs(refPointTwo[2] - x1) < frame.shape[0]/5 and abs(refPointTwo[3] - y1) < frame.shape[0]/5) or (abs(refPointTwo[2] - x2) < frame.shape[0]/5 and abs(refPointTwo[3] - y2) < frame.shape[0]/5):
+            return nearTrueMid(x1, y1, x2, y2, (0, 0, 255))
+
+    def nearTrueMid(x1, y1, x2, y2, colour):
+        #cv2.line(frame, (x1, y1), (x2, y2), (255, 255, 255), 2)
+        if (abs(midPointCoord[0] - x1) < frame.shape[0]/5 and abs(midPointCoord[1] - y1) < frame.shape[0]/5)or (abs(midPointCoord[0] - x2) < frame.shape[0]/5 and abs(midPointCoord[1] - y2) < frame.shape[0]/5):
+            currentLineSlope = slopeCheck(x1, y1, x2, y2)
+            #right
+            if y1 < midPointCoord[1]:
+                if slopeCheck(x1 + int(abs(y1-midPointCoord[1])/currentLineSlope), midPointCoord[1], x2, y2) > 0.5:
+                    cv2.line(frame, (x1 + int(abs(y1-midPointCoord[1])/currentLineSlope), midPointCoord[1]), (x2 + int(abs(y2-midPointCoord[3])/currentLineSlope), midPointCoord[3]), colour, 2)
+                    return(x1 + int(abs(y1-midPointCoord[1])/currentLineSlope), midPointCoord[1], x2 + int(abs(y2-midPointCoord[3])/currentLineSlope), midPointCoord[3])
+
+            #left
+            elif y2 < midPointCoord[1]:
+                if slopeCheck(x1, y1, x2 - int(abs(y2-midPointCoord[1])/currentLineSlope), midPointCoord[1]) > 0.5:
+                    cv2.line(frame, (x2 - int(abs(y2-midPointCoord[3])/currentLineSlope), midPointCoord[3]), (x2 - int(abs(y2-midPointCoord[1])/currentLineSlope), midPointCoord[1]), colour, 2) 
+                    return (x2 - int(abs(y2-midPointCoord[1])/currentLineSlope), midPointCoord[1], x2 - int(abs(y2-midPointCoord[3])/currentLineSlope), midPointCoord[3])
+
+    def slopeCheck(x1, y1, x2, y2):
+        if math.isinf(round(abs(y1-y2)/abs(x1-x2), 2)):
+            return 10000
+        else:
+            return round(abs(y1-y2)/abs(x1-x2), 2)
+
+    def tempCheck(temp):
+        real = True
+        if temp is not None and len(frameArray) == 1:
+            for value in range(0, len(frameArray), 2):
+                if abs(int(frameArray[0][value]) - int(temp[value])) > frame.shape[1]/50:
+                    pass
+                else:
+                    real = False
+            if real:
+                return temp
+        elif temp is not None and len(frameArray) == 0:
+            return temp
+
+    cap = cv2.VideoCapture('codeFiles/roadVideos/gitHubVideo1.mp4')
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
+            midPointCoord = [int(frame.shape[1]/2), int(frame.shape[0]/2)+int(frame.shape[0]/10), int(frame.shape[1]/2), int(frame.shape[0])]
+            refPointOne = [int(frame.shape[1]/2)-int(frame.shape[1]/25), int(frame.shape[0]/2)+int(frame.shape[0]/10), int(frame.shape[1]/4), int(frame.shape[0])]
+            refPointTwo = [int(frame.shape[1]/2)+int(frame.shape[1]/25), int(frame.shape[0]/2)+int(frame.shape[0]/10), int(frame.shape[1]/4)*3, int(frame.shape[0])]
+
+            frame = cv2.GaussianBlur(frame, (7, 7), 0)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            edges = cv2.Canny(gray, 120, 200, apertureSize=3)
+            lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi/180, threshold=50, minLineLength=0, maxLineGap=frame.shape[1])
+
+            frameArray = []
+
+            for line in lines:
+                x1, y1, x2, y2 = line[0]
+                temp = tempCheck(nearBy(x1, y1, x2, y2))
+                if temp is not None:
+                    frameArray.append(temp)
+                        
+            x1, y1, x2, y2 = 0, 0, 0, 0
+            for value in frameArray:
+                x1, y1, x2, y2 = int(value[0]) + x1, int(value[1]) + y1, int(value[2]) + x2, int(value[3]) + y2
+            if len(frameArray) == 2:
+                x1, y1, x2, y2 = x1/len(frameArray), y1/len(frameArray), x2/len(frameArray), y2/len(frameArray)
+                cv2.line(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+
+            _, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
             
 @app.route('/video_feed')
 def video_feed():
