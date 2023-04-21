a6e363fcaea2dadd139662a713dea277805a4024
pl244783 <pl244783@ahschool.com>
2023-04-20 21:32:48 -0400

    unrevamped

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ class ExcludeFilter(logging.Filter):
         return "GET /static/logFile.html" not in message and "GET /video_feed?rand=" not in message
 
 logger = logging.getLogger()
-handler = logging.FileHandler('HTMLFiles/static/logFile.html', 'w')
+handler = logging.FileHandler('static/logFile.html', 'w')
 handler.setLevel(logging.INFO)
 handler.addFilter(ExcludeFilter())
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
@@ -24,7 +24,7 @@ logger.addHandler(handler)
 
 # Initialize the database
 def init_db():
-    conn = sqlite3.connect('HTMLFiles/static/user_data.db')
+    conn = sqlite3.connect('static/user_data.db')
     c = conn.cursor()
 
     # Create the users table if it doesn't exist
@@ -55,7 +55,7 @@ def login():
         password = request.form['password']
 
         # Check if the username and password match
-        conn = sqlite3.connect('HTMLFiles/static/user_data.db')
+        conn = sqlite3.connect('static/user_data.db')
         c = conn.cursor()
         c.execute('SELECT * FROM users WHERE username=? AND password=?', (hashlib.sha256(username.encode()).hexdigest(), hashlib.sha256(password.encode()).hexdigest()))
         result = c.fetchone()
@@ -75,7 +75,7 @@ def register():
         validUsername = True
 
         # Check if the username is available
-        conn = sqlite3.connect('HTMLFiles/static/user_data.db')
+        conn = sqlite3.connect('static/user_data.db')
         c = conn.cursor()
         c.execute('SELECT * FROM users WHERE username=?', (username,))
         result = c.fetchone()
@@ -142,7 +142,7 @@ def gen_frames():
         elif temp is not None and len(frameArray) == 0:
             return temp
 
-    cap = cv2.VideoCapture('codeFiles/roadVideos/gitHubVideo1.mp4')
+    cap = cv2.VideoCapture('roadVideos/gitHubVideo1.mp4')
     while cap.isOpened():
         ret, frame = cap.read()
         if ret:
